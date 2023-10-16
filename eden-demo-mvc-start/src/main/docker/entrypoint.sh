f87077d689a56d43ad371d3e4df15400ba5b43b1
guoyuanlu <guoyuanlu@puyiwm.com>
2023-10-16 20:58:41 +0800

    update

--- eden-demo-mvc-start/src/main/docker/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "The application will start in ${JAVA_SLEEP}s..." && sleep ${JAVA_SLEEP}
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "org.ylzl.eden.demo.MvcApplication" "$@"
